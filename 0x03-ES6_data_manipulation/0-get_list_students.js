66aaabaee8f0cca403002c5f7d4970193bb059f1
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-07-01 08:31:32 +0300

    Task(0): Done

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+  const obj1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const obj2 = { id: 2, firstName: 'James', location: 'Columbia' };
+  const obj3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  return [obj1, obj2, obj3];
+}
+
+// console.log(getListStudents());
